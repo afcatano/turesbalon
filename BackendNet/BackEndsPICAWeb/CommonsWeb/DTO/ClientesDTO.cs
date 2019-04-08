0b4f779eb3affb1b3040e9b60d6a9115b3a3e46e
jeanvees <49243604+jeanvees@users.noreply.github.com>
2019-04-07 22:28:46 -0500

    Metodo equals para poder validar cache

--- BackendNet/BackEndsPICAWeb/CommonsWeb/DTO/ClientesDTO.cs
+++ BackendNet/BackEndsPICAWeb/CommonsWeb/DTO/ClientesDTO.cs
@@ -20,7 +20,27 @@ namespace BackEndsPICAWeb.Business.Clientes.DTO
         public string User { get; set; }
         public string Password { get; set; }
         public string Status { get; set; }
-        public List<CreditCardDTO>  LCreditCard { get; set; }
+        public List<CreditCardDTO> LCreditCard { get; set; }
+
+        public override bool Equals(object obj)
+        {
+
+            ClientesDTO lc_cliente;
+
+            lc_cliente = obj as ClientesDTO;
+
+            if (lc_cliente == null)
+                return false;
+
+            if (CustID != lc_cliente.CustID || FName != lc_cliente.FName || LName != lc_cliente.LName ||
+                Email != lc_cliente.Email || PhoneNumber != lc_cliente.PhoneNumber ||
+                Address != lc_cliente.Address || City != lc_cliente.City || Country != lc_cliente.Country ||
+                User != lc_cliente.User || Status != lc_cliente.Status || Password != lc_cliente.Password)
+                return false;
+
+            return true;
+        }
+
 
     }
 }
